4e9d6dbf5c38863db4277d975e4e5d74e6aa4603
José Manuel Guillamón <josemanuel.guillamon@gmail.com>
2024-10-10 12:56:01 +0200

    He añadido un comentario

--- src/clases/PedirMesMientrasSeaIncorrecto.java
+++ src/clases/PedirMesMientrasSeaIncorrecto.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PedirMesMientrasSeaIncorrecto {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		
+		// Declaro la variable String
 		String sMes = "";
 		boolean mesCorrecto; 
 		do {
